cf99c36134a371fe98555e3e28f1645cae530573
carlospinheiro <cadutapinheiro@gmail.com>
2023-11-23 19:28:17 -0300

    change img attribute

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function toggleMode() {
     const html = document.documentElement;
     html.classList.toggle("light");
+
+    const img = document.querySelector("#profile img");
+
+    if(html.classList.contains("light")) {
+        img.setAttribute('src', './assets/perfil.jpg')
+    } else {
+        img.setAttribute('src', './assets/fotoperfilhome-removebg-preview.png')
+    }
 }
